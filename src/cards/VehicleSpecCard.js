73381cea94809f940c57cc740e01b93acf342582
xf4c70r <arvindsudarshan18@gmail.com>
2023-12-18 12:11:35 -0800

    Sign Up Page

---
+++ src/cards/VehicleSpecCard.js
@@ -0,0 +1,55 @@
+import React from "react";
+// reactstrap components
+import{
+  Card,
+  CardImg,
+  CardImgOverlay,
+  CardTitle,
+  CardText
+} from "reactstrap";
+// core components
+
+function VehicleSpecCard(){
+
+    const vehicleSpecs = {
+        Engine: "V8 Turbo",
+        VIN: "1HGBH41JXMN109186",
+        ChasisNumber: "MN109186S",
+        Seats: 5,
+        TypeOfDrive: "AWD",
+        FuelType: "Petrol",
+        ClassOfVehicle: "SUV",
+        ModelName: "X5",
+        CarMake: "BMW",
+        Year: 2021
+      };
+    
+  return (
+    <>
+      <Card className="bg-dark">
+        <CardImg
+          alt="..."
+          data-src="holder.js/100px270/#55595c:#373a3c/text:Card image"
+        ></CardImg>
+        <CardImgOverlay>
+          <CardTitle tag="h4">Vehicle Specifications</CardTitle>
+          <CardText>
+            <Table dark bordered responsive>
+              <tbody>
+                {Object.entries(vehicleSpecs).map(([key, value]) => (
+                  <tr key={key}>
+                    <th scope="row">{key}</th>
+                    <td>{value}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          </CardText>
+          <CardText>Last updated 3 mins ago</CardText>
+        </CardImgOverlay>
+      </Card>
+    </>
+  );
+}
+
+export default VehicleSpecCard
