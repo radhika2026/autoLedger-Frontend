e89eabd35d37754ff61de8176f45c29f8f22004d
xf4c70r <arvindsudarshan18@gmail.com>
2023-12-18 14:13:34 -0800

    Vehicle Info Page

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ import LoginPage from "views/examples/LoginPage.js";
 import LandingPage from "views/examples/LandingPage.js";
 import ProfilePage from "views/examples/ProfilePage.js";
 import Insurance from "views/examples/Insurance.js";
+import VehicleInformationPage from "views/examples/VehicleInformationPage";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
@@ -25,6 +26,7 @@ root.render(
       <Route path="/profile-page" element={<ProfilePage />} />
       <Route path="/login-page" element={<LoginPage />} />
       <Route path="/insurance" element={<Insurance />} />
+      <Route path="/vehicle-info" element={<VehicleInformationPage />} />
 
       <Route path="*" element={<Navigate to="/index" replace />} />
     </Routes>
